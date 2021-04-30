4fa8bf308f59ec903c60f4051b405f9566d0d878
Jules Scheil <juliana.m.scheil@gmail.com>
2021-04-30 16:45:16 -0400

    added schema

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001
+
+app.use(express.static(path.join(__dirname, 'public')));
+// Sets up the routes
+app.use(require('./controllers/dish-routes'));
 
 app.listen(PORT, () => console.log('Now listening'));
